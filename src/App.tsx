258ec98c5350ecf58bcfefa4aef27b4cb43d212e
Tampoka <goal.thanks@gmail.com>
2022-05-15 16:49:04 +0900

    Add styled-components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import {CSSProperties} from 'react';
+
+const buttonStyles: CSSProperties = {
+    backgroundColor: "#5aac44",
+    borderRadius: "3px",
+    border: "none",
+    boxShadow: "none"
+}
 
 export function App() {
   return (
     <div className="App">
      Trello
+        <button style={buttonStyles}>Click me</button>
     </div>
   );
 }
