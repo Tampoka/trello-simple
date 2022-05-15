5c16d4ad16270a748363da006f45453f8b994d55
Tampoka <goal.thanks@gmail.com>
2022-05-15 22:53:19 +0900

    Add AddItemButton.

--- src/styles.ts
+++ src/styles.ts
@@ -10,26 +10,46 @@ export const AppContainer = styled.div`
   width: 100%;
 `
 export const ColumnContainer = styled.div`
-background-color: #ebecf0;
-width: 300px;
-min-height: 120px;
-margin-right: 20px;
-border-radius: 3px;
-padding: 8px 8px;
-flex-grow: 0;
+  background-color: #ebecf0;
+  width: 300px;
+  min-height: 120px;
+  margin-right: 20px;
+  border-radius: 3px;
+  padding: 8px 8px;
+  flex-grow: 0;
 `
 export const ColumnTitle = styled.div`
   padding: 6px 16px 12px;
   font-weight: bold;
   color: #d354d7;
 `
-
 export const CardContainer = styled.div`
-background-color: #fff;
-cursor: pointer;
-margin-bottom: 0.5rem;
-padding: 0.5rem 1rem;
-max-width: 300px;
-border-radius: 3px;
-box-shadow: #091e4240 0 1px 0 0;
+  background-color: #fff;
+  cursor: pointer;
+  margin-bottom: 0.5rem;
+  padding: 0.5rem 1rem;
+  max-width: 300px;
+  border-radius: 3px;
+  box-shadow: #091e4240 0 1px 0 0;
 `
+
+type AddItemButtonProps = {
+    dark?: boolean
+}
+
+export const AddItemButton = styled.button<AddItemButtonProps>`
+  background-color: #ffffff3d;
+  border-radius: 3px;
+  border: none;
+  color: ${props => (props.dark ? "#000" : "#fff")};
+  cursor: pointer;
+  max-width: 300px;
+  padding: 10px 12px;
+  text-align: left;
+  transition: background 85ms ease-in;
+  width: 100%;
+  &:hover {
+    background-color: #ffffff52;
+  }
+`
+
