b641376c7c91b555d4175360388605cfcf205a03
Tampoka <goal.thanks@gmail.com>
2022-05-16 22:14:37 +0900

    Modify ColumnContainer, CardContainer to extend DragPreviewContainer.

--- src/styles.ts
+++ src/styles.ts
@@ -9,29 +9,10 @@ export const AppContainer = styled.div`
   padding: 20px;
   width: 100%;
 `
-export const ColumnContainer = styled.div`
-  background-color: #ebecf0;
-  width: 300px;
-  min-width: 100px;
-  min-height: 120px;
-  margin-right: 20px;
-  border-radius: 3px;
-  padding: 8px 8px;
-  flex-grow: 0;
-`
 export const ColumnTitle = styled.div`
   padding: 6px 16px 12px;
   font-weight: bold;
   color: #d354d7;
-`
-export const CardContainer = styled.div`
-  background-color: #fff;
-  cursor: pointer;
-  margin-bottom: 0.5rem;
-  padding: 0.5rem 1rem;
-  max-width: 300px;
-  border-radius: 3px;
-  box-shadow: #091e4240 0 1px 0 0;
 `
 
 type AddItemButtonProps = {
@@ -89,4 +70,23 @@ interface DragPreviewContainerProps {
 
 export const DragPreviewContainer = styled.div<DragPreviewContainerProps>`
   opacity: ${props => (props.isHidden ? 0.3 : 1)}
+`
+export const ColumnContainer = styled(DragPreviewContainer)`
+  background-color: #ebecf0;
+  width: 300px;
+  min-width: 100px;
+  min-height: 120px;
+  margin-right: 20px;
+  border-radius: 3px;
+  padding: 8px 8px;
+  flex-grow: 0;
+`
+export const CardContainer = styled(DragPreviewContainer)`
+  background-color: #fff;
+  cursor: pointer;
+  margin-bottom: 0.5rem;
+  padding: 0.5rem 1rem;
+  max-width: 300px;
+  border-radius: 3px;
+  box-shadow: #091e4240 0 1px 0 0;
 `
