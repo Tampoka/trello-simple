d96f1289f0893cc22b63f6d082611401fe4c2815
Tampoka <goal.thanks@gmail.com>
2022-05-16 16:34:46 +0900

    Add tasks.

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,6 +1,8 @@
 import {FC, ReactNode} from 'react';
 import {ColumnContainer, ColumnTitle} from '../styles';
 import {AddNewItem} from './AddNewItem';
+import {useAppState} from '../state/AppStateContext';
+import {Card} from './Card';
 
 /*type PropsWithChildren<P> = P & {
     children?: ReactNode;
@@ -8,14 +10,19 @@ import {AddNewItem} from './AddNewItem';
 
 type ColumnProps = {
     text?: string
-    id:string
-    children?: ReactNode
+    id: string
 }
-export const Column: FC<ColumnProps> = ({text, id,children}) => {
+export const Column: FC<ColumnProps> = ({text, id}) => {
+    const {getTasksByListId} = useAppState()
+
+    const tasks = getTasksByListId(id)
+
     return (
         <ColumnContainer>
             <ColumnTitle>{text}</ColumnTitle>
-            {children}
+            {tasks.map(task => (
+                <Card id={task.id} text={task.text} key={task.id}/>
+            ))}
             <AddNewItem
                 toggleButtonText="+ Add another task"
                 onAdd={console.log}
